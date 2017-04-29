81cb80d87cfb197a18f5b0f9cb5e07b025cb164a
Alex Menor <alex4menor@gmail.com>
2017-04-29 03:01:48 +0200

    deberia funcionar

--- line2.py
+++ line2.py
@@ -1,5 +1,6 @@
 from rrb3 import *
 import RPi.GPIO as GPIO
+import time
 rr = RRB3(5, 4)
 izquierda = 2
 derecha = 3
@@ -10,13 +11,15 @@ GPIO.setup(10, GPIO.OUT)
 GPIO.setup(25, GPIO.OUT)
 GPIO.setup(17, GPIO.OUT)
 GPIO.setup(4, GPIO.OUT)
-
+#rr.forward()
+#time.sleep(0.2)
 while True:
-		rr.set_motors(0.3,0,0.3,0)
+		rr.set_motors(0.2,0,0.2,0)
 		if GPIO.input(derecha) == GPIO.LOW and GPIO.input(izquierda) == GPIO.LOW:
-			rr.set_motors(0.1,0,0.1,0)
-		elif GPIO.input(izquierda) == GPIO.HIGH:
-			rr.set_motors(0.4,0,0,0)
+			rr.set_motors(0.4,0,0.4,0)
 		elif GPIO.input(derecha) == GPIO.HIGH:
 			rr.set_motors(0,0,0.4,0)
-		
+			time.sleep(0.2)
+		elif GPIO.input(izquierda) == GPIO.HIGH:
+			rr.set_motors(0.4,0,0,0)
+			time.sleep(0.2)
