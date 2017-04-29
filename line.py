e2f1f0fb5cb9425b72a7836218fcb2455c3fa750
Alex Menor <alex4menor@gmail.com>
2017-04-29 12:39:49 +0200

    Programa totalmente funcional seguidor de linea

--- line.py
+++ line.py
@@ -1,14 +1,13 @@
-#Librerias
+#Librerias necesarias
 from rrb3 import *
 import RPi.GPIO as GPIO
-import sys
-import tty
-import termios
 import time
+#Voltaje de la placa
 rr = RRB3(5, 4)
+#Puertos GPIO de los seguidores de linea
 izquierda = 2
 derecha = 3
-lee2=""
+#Configuramos los puertos GPIO a utilizar
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(izquierda, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(derecha, GPIO.IN, pull_up_down=GPIO.PUD_UP)
@@ -16,53 +15,18 @@ GPIO.setup(10, GPIO.OUT)
 GPIO.setup(25, GPIO.OUT)
 GPIO.setup(17, GPIO.OUT)
 GPIO.setup(4, GPIO.OUT)
-#Mapeo de teclas y sensores
-empezar= "w"
-salida = "p"
-
-#Funciones para leer el teclado
-def readchar():
-    fd = sys.stdin.fileno()
-    old_settings = termios.tcgetattr(fd)
-    try:
-        tty.setraw(sys.stdin.fileno())
-        ch = sys.stdin.read(1)
-    finally:
-        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-    if ch == '0x03':
-        raise KeyboardInterrupt
-    return ch
-
-def readkey(getchar_fn=None):
-    getchar = getchar_fn or readchar
-    c1 = getchar()
-    if ord(c1) != 0x1b:
-        return c1
-    c2 = getchar()
-    if ord(c2) != 0x5b:
-        return c1
-    c3 = getchar()
-    return ord(c3) - 65  
-
 #Programa principal
-try: 
-    while True:
-        LEER = readkey()
-        if LEER == empezar:
-            print("Arranca")
-            while lee2!=salida:
-        		lee2 = readkey()
-        		if GPIO.input(derecha) == GPIO.LOW and GPIO.input(izquierda) == GPIO.LOW:
-        			rr.set_motors(0.5,0,0.5,0)
-    			elif GPIO.input(derecha) == GPIO.HIGH:
-    				rr.set_motors(0,0,0.5,0)
-				elif GPIO.input(izquierda) == GPIO.HIGH:
-					rr.set_motors(0.5,0,0,0)
-			raise KeyboardInterrupt
-        elif LEER == salida:
-            raise KeyboardInterrupt
-    
-#En caso de pulsar la tecla de salida
+try:
+	while True:
+		if GPIO.input(derecha) == GPIO.LOW and GPIO.input(izquierda) == GPIO.LOW:
+				rr.set_motors(0.5,0,0.5,0)
+		elif GPIO.input(derecha) == GPIO.HIGH:
+				rr.set_motors(0,0,0.5,0)
+				time.sleep(0.05)
+		elif GPIO.input(izquierda) == GPIO.HIGH:
+				rr.set_motors(0.5,0,0,0)
+				time.sleep(0.05)
+#Sale del programa con CTRL + C
 except KeyboardInterrupt:
-    print("Saliendo del seguimiento de linea")
-    GPIO.cleanup()
+	print("Sailendo del seguidor de linea")
+	GPIO.cleanup()
