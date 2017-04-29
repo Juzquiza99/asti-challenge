fe7b80614f14edfb3bdbe2947247847ab3ce9c5a
Alex Menor <alex4menor@gmail.com>
2017-04-29 02:24:36 +0200

    mas o menos funciona

--- line2.py
+++ line2.py
@@ -10,14 +10,13 @@ GPIO.setup(10, GPIO.OUT)
 GPIO.setup(25, GPIO.OUT)
 GPIO.setup(17, GPIO.OUT)
 GPIO.setup(4, GPIO.OUT)
-GPIO.setup(14, GPIO.OUT)
-GPIO.setup(24, GPIO.OUT)
 
 while True:
 		rr.set_motors(0.3,0,0.3,0)
 		if GPIO.input(derecha) == GPIO.LOW and GPIO.input(izquierda) == GPIO.LOW:
-			rr.set_motors(0.3,0,0.3,0)
-		elif GPIO.input(derecha) == GPIO.HIGH:
-			rr.set_motors(0.3,0,0.1,0)
+			rr.set_motors(0.1,0,0.1,0)
 		elif GPIO.input(izquierda) == GPIO.HIGH:
-			rr.set_motors(0.1,0,0.3,0)
+			rr.set_motors(0.4,0,0,0)
+		elif GPIO.input(derecha) == GPIO.HIGH:
+			rr.set_motors(0,0,0.4,0)
+		
