5bf2033d08fe6429897502aafb512860182bc477
Alex Menor <alex4menor@gmail.com>
2017-04-29 12:31:31 +0200

    Fallaba un cable xd

--- line3.py
+++ line3.py
@@ -11,14 +11,16 @@ GPIO.setup(10, GPIO.OUT)
 GPIO.setup(25, GPIO.OUT)
 GPIO.setup(17, GPIO.OUT)
 GPIO.setup(4, GPIO.OUT)
-
-while True:
-	if GPIO.input(derecha) == GPIO.LOW and GPIO.input(izquierda) == GPIO.LOW:
-			rr.set_motors(0.5,0,0.5,0)
-	elif GPIO.input(derecha) == GPIO.HIGH:
-			rr.set_motors(0,0,0.5,0)
-			time.sleep(0.05)
-	elif GPIO.input(izquierda) == GPIO.HIGH:
-			rr.set_motors(0.5,0,0,0)
-			time.sleep(0.05)
-
+try:
+	while True:
+		if GPIO.input(derecha) == GPIO.LOW and GPIO.input(izquierda) == GPIO.LOW:
+				rr.set_motors(0.5,0,0.5,0)
+		elif GPIO.input(derecha) == GPIO.HIGH:
+				rr.set_motors(0,0,0.5,0)
+				time.sleep(0.05)
+		elif GPIO.input(izquierda) == GPIO.HIGH:
+				rr.set_motors(0.5,0,0,0)
+				time.sleep(0.05)
+except KeyboardInterrupt:
+	print("Sailendo del seguidor de linea")
+	GPIO.cleanup()
