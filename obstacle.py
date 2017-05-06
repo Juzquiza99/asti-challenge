22025a436ff78638fec5dd347c63e6086c018c1d
Alex Menor <alex4menor@gmail.com>
2017-05-06 05:35:48 +0200

    Listos para el challenge

--- obstacle.py
+++ obstacle.py
@@ -12,16 +12,31 @@ try:
 		dist = rr.get_distance()
 		d = rr.get_distancd()
 		i = rr.get_distanci()
-		if dist>8:
-			rr.set_motors(0.2,0,0.2,0)
-		elif dist<8:
-			rr.stop()
-		elif d<8:
-			rr.set_motors(0,0.2,0,0)
-			time.sleep(1)
-		elif i<8:
-			rr.set_motors(0,0,0.2,0)
-			time.sleep(1)
+		if dist > 20:
+			rr.set_motors(0.3,0,0.3,0)
+		if dist < 20:
+			rr.reverse()
+			time.sleep(0.3)
+			if rr.get_distanci>rr.get_distancd:
+				while True:
+					rr.left()
+					time.sleep(0.2)
+					if rr.get_distance() > 20:
+						break
+
+			else:
+				while True:
+					rr.right()
+					time.sleep(0.2)
+					if rr.get_distance() > 20:
+						break
+
+		if i < 15:
+			rr.set_motors(0.3,0,0,0)
+			time.sleep(0.1)
+		if d < 15:
+			rr.set_motors(0,0,0.3,0)
+			time.sleep(0.1)
 			
 
 except KeyboardInterrupt:
