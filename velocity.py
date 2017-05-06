d89367e4280ae9bcffbf29f8486fc671cce59c16
AlexMenor <alex4menor@gmail.com>
2017-05-06 09:40:39 +0200

    line

--- velocity.py
+++ velocity.py
@@ -3,7 +3,7 @@ from rrb3 import *
 rr=RRB3(9, 8)
 try:
 	while True:
-		rr.set_motors(1,0,1,0)
+		rr.set_motors(0.9,0,1,0)
 		rr.set_oc1(1)
 #Al salir del programa, se para el vehiculo y el ventilador propulsor
 finally:
